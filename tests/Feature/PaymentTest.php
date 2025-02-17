e197bbd58487e4a437203720f17a290700f17944
Victor Ventura <euventura@gmail.com>
2025-02-17 08:48:14 -0300

    improving tests

--- tests/Feature/PaymentTest.php
+++ tests/Feature/PaymentTest.php
@@ -2,11 +2,15 @@
 
 namespace Tests\Feature;
 
+use App\Http\Controllers\PaymentController;
+use App\Http\Integrations\Asaas\AsaasConnector;
+use App\Http\Requests\PaymentRequest;
+use App\Models\Payment;
 use Saloon\Http\Faking\MockClient;
 use Saloon\Http\Faking\MockResponse;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-
+use Illuminate\View\View;
 
 class PaymentTest extends TestCase
 {
@@ -20,58 +24,82 @@ class PaymentTest extends TestCase
         $response->assertStatus(302);
     }
 
-    public function test_success(): void
+    public function test_exception(): void
     {
-        $paymentResponse = include 'paymentResponse.php';
+        $this->expectException(\Illuminate\Validation\ValidationException::class);
+        $paymentResponse = include 'paymentResponseException.php';
         $mockClient = new MockClient([
-            '*/v3/payments' => MockResponse::make(body: $paymentResponse, status: 200),
+            '*/v3/payments' => MockResponse::make(body: $paymentResponse, status: 200)
         ]);
 
-        $response = $this->post('/payment', [
-            'customer' => '6519801',
-            'billing_type' => 'BOLETO',
-            'value' => 2222.0,
-            'due_date' => '2025-02-21',
-            'description' => 'teste']);
+        $asaasConnector = new AsaasConnector();
+        $asaasConnector->withMockClient($mockClient);
+        $controller = new PaymentController(new Payment(), $asaasConnector);
 
-        $response->assertStatus(200);
-
-    }
-
-    public function test_invalid_due_date(): void
-    {
-        $paymentResponse = include 'paymentResponse.php';
-        $mockClient = new MockClient([
-            '*/v3/payments' => MockResponse::make(body: $paymentResponse, status: 200),
-        ]);
-
-        $response = $this->post('/payment', [
+        
+        $paymentRequest = new PaymentRequest();
+        $paymentRequest->merge([
             'customer' => '6519801',
             'billing_type' => 'BOLETO',
             'value' => 2222.0,
             'due_date' => '2024-02-21',
             'description' => 'teste']);
+        $response = $controller->makePayment($paymentRequest);
+dd($response);
+        // $this->assertEquals(resource_path('views/sucesso.blade.php'), $response->getPath());
+        // $this->assertInstanceOf(View::class, $response);
+    }
 
-        $response->assertStatus(302);
+    public function test_success(): void
+    {
+        $paymentResponse = include 'paymentResponse.php';
+        $mockClient = new MockClient([
+            '*/v3/payments' => MockResponse::make(body: $paymentResponse, status: 200)
+        ]);
+
+        $asaasConnector = new AsaasConnector();
+        $asaasConnector->withMockClient($mockClient);
+        $controller = new PaymentController(new Payment(), $asaasConnector);
+
+        
+        $paymentRequest = new PaymentRequest();
+        $paymentRequest->merge([
+            'customer' => '6519801',
+            'billing_type' => 'BOLETO',
+            'value' => 2222.0,
+            'due_date' => '2025-02-21',
+            'description' => 'teste']);
+        $response = $controller->makePayment($paymentRequest);
+
+        $this->assertEquals(resource_path('views/sucesso.blade.php'), $response->getPath());
+        $this->assertInstanceOf(View::class, $response);
 
     }
 
     public function test_pix(): void
     {
-        $paymentResponse = include 'paymentResponse.php';
-
+        $paymentResponse = include 'paymentResponsePix.php';
         $mockClient = new MockClient([
             '*/v3/payments' => MockResponse::make(body: $paymentResponse, status: 200),
             '*/v3/payments/*/pixQrCode' => MockResponse::make(body: '', status: 200),
         ]);
 
-        $response = $this->post('/payment', [
+        $asaasConnector = new AsaasConnector();
+        $asaasConnector->withMockClient($mockClient);
+        $controller = new PaymentController(new Payment(), $asaasConnector);
+
+        
+        $paymentRequest = new PaymentRequest();
+        $paymentRequest->merge([
             'customer' => '6519801',
             'billing_type' => 'PIX',
             'value' => 2222.0,
             'due_date' => '2025-02-21',
             'description' => 'teste']);
+        $response = $controller->makePayment($paymentRequest);
 
-        $response->assertStatus(200);
+        $this->assertEquals(resource_path('views/pix.blade.php'), $response->getPath());
+        $this->assertInstanceOf(View::class, $response);
     }
+
 }
---
+++ tests/Feature/paymentResponseException.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'errors' => [
+        ['code' => '1',
+        'description' => 'this text has to exists']
+    ],
+    [
+        ['code' => '2',
+        'description' => 'this text has to exists too']
+    ]
+];
